ad3eae08caaaefbbc544c57bb4ff9266a43cdf80
aiapo <agitschl@umflint.edu>
2023-08-25 12:20:59 -0400

    Add more attributes to CSV

--- src/main/java/com/troxal/CSVManip.java
+++ src/main/java/com/troxal/CSVManip.java
@@ -15,18 +15,33 @@ public class CSVManip {
         {
             CSVWriter writer = new CSVWriter(new FileWriter(file));
 
-            String[] headerTxt = {"Github ID","Repository Name","Github Link","Creation Date","Mentionable Users","Total Size","Total Commits"};
+            String[] headerTxt = {"Github ID","Repository Name","Github Link","Description","Primary Language","Creation Date","Update Date","Push Date","Mentionable Users","Issue Users","Total Size","Total Commits","Forks","Stars","Watchers","Languages"};
             writer.writeNext(headerTxt);
 
             for(int i=0;i<repos.size();i++){
+                StringBuilder sb = new StringBuilder();
+                for(int j=0;i<repos.get(i).getLanguages().size();j++)
+                    sb.append(repos.get(i).getLanguages().get(j).toString());
+
                 String tempLine[] = new String[]{
                         repos.get(i).getId(),
                         repos.get(i).getName(),
                         repos.get(i).getUrl(),
+                        repos.get(i).getDescription(),
+                        repos.get(i).getPrimaryLanguage(),
                         repos.get(i).getCreationDate(),
+                        repos.get(i).getUpdateDate(),
+                        repos.get(i).getPushDate(),
+                        String.valueOf(repos.get(i).getIsArchived()),
+                        String.valueOf(repos.get(i).getIsFork()),
                         String.valueOf(repos.get(i).getTotalMentionableUsers()),
+                        String.valueOf(repos.get(i).getTotalIssueUsers()),
                         String.valueOf(repos.get(i).getTotalProjectSize()),
-                        String.valueOf(repos.get(i).getTotalCommits())
+                        String.valueOf(repos.get(i).getTotalCommits()),
+                        String.valueOf(repos.get(i).getForkCount()),
+                        String.valueOf(repos.get(i).getStarCount()),
+                        String.valueOf(repos.get(i).getWatchCount()),
+                        sb.toString()
                 };
                 writer.writeNext(tempLine);
             }
