3e045159eff4b0e8d0a26d2b329df2e7f7133a1c
karld20 <139264991+karld20@users.noreply.github.com>
2023-08-30 03:45:59 -0400

    New Query menu option, loop to main method

--- src/main/java/com/troxal/Main.java
+++ src/main/java/com/troxal/Main.java
@@ -6,127 +6,134 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Integer menuChoice = 0;
-        Boolean success = false;
+        boolean newQuery = true;
+        while (newQuery) {
+            Integer menuChoice = 0;
 
-        System.out.println("-- Welcome to RepoGrabber! --\n");
-        Scanner scn = new Scanner(System.in);
-        System.out.println("What is the minimum amount of followers on project wanted? (ex: '50'): ");
-        Integer followers=0;
-        while(!success){
-            try {
-                followers = Integer.valueOf(scn.next());
-                success = true;
-            }catch(NumberFormatException e){
-                System.out.println("Invalid, please enter a number (ex: '50'): ");
-            }
-        }
-        System.out.println("What language do you want to grab? (ex: 'java'): ");
-        String languages="";
-        success=false;
-        while(!success){
-            try {
-                languages = scn.next();
-                success = true;
-            }catch(NumberFormatException e){
-                System.out.println("Invalid, please enter a string (ex: 'java'): ");
-            }
-        }
-        System.out.println("What is the minimum amount of mentionable users wanted? (ex: '20'): ");
-        Integer users = 0;
-        success=false;
-        while(!success){
-            try {
-                users = Integer.valueOf(scn.next());
-                success = true;
-            }catch(NumberFormatException e){
-                System.out.println("Invalid, please enter a number (ex: '20'): ");
-            }
-        }
-        System.out.println("What is the percentage of the language in the repo wanted? (ex. '51' means at >=51% is language): ");
-        Integer percentLanguage=0;
-        success=false;
-        while(!success){
-            try {
-                percentLanguage = Integer.valueOf(scn.next());
-                success = true;
-            }catch(NumberFormatException e){
-                System.out.println("Invalid, please enter a number (ex: '51'): ");
-            }
-        }
-        System.out.println("What is minimum amount of commits wanted? (ex. '300'): ");
-        Integer totalCommit=0;
-        success=false;
-        while(!success){
-            try {
-                totalCommit = Integer.valueOf(scn.next());
-                success = true;
-            }catch(NumberFormatException e){
-                System.out.println("Invalid, please enter a number (ex: '300'): ");
-            }
-        }
-        System.out.println("What is minimum size in bytes of repo wanted? (ex. '5000'): ");
-        Integer totalSize=0;
-        success=false;
-        while(!success){
-            try {
-                totalSize = Integer.valueOf(scn.next());
-                success = true;
-            }catch(NumberFormatException e){
-                System.out.println("Invalid, please enter a number (ex: '5000'): ");
-            }
-        }
-        System.out.println("What is start date of repos wanted? (ex. '2010-01-01'): ");
-        String sDate="";
-        success=false;
-        while(!success){
-            try {
-                sDate = scn.next();
-                success = true;
-            }catch(NumberFormatException e){
-                System.out.println("Invalid, please enter a string (ex: '2010-01-01'): ");
-            }
-        }
+            Boolean success = false;
 
-        System.out.println("\n** Grabbing repos!");
-        RepoGrab rg = new RepoGrab(followers,languages,users,percentLanguage,totalCommit,totalSize,sDate);
-        System.out.println("\n** Grabbed repos!");
+            System.out.println("-- Welcome to RepoGrabber! --\n");
+            Scanner scn = new Scanner(System.in);
+            System.out.println("What is the minimum amount of followers on project wanted? (ex: '50'): ");
+            Integer followers = 0;
+            while (!success) {
+                try {
+                    followers = Integer.valueOf(scn.next());
+                    success = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid, please enter a number (ex: '50'): ");
+                }
+            }
+            System.out.println("What language do you want to grab? (ex: 'java'): ");
+            String languages = "";
+            success = false;
+            while (!success) {
+                try {
+                    languages = scn.next();
+                    success = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid, please enter a string (ex: 'java'): ");
+                }
+            }
+            System.out.println("What is the minimum amount of mentionable users wanted? (ex: '20'): ");
+            Integer users = 0;
+            success = false;
+            while (!success) {
+                try {
+                    users = Integer.valueOf(scn.next());
+                    success = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid, please enter a number (ex: '20'): ");
+                }
+            }
+            System.out.println("What is the percentage of the language in the repo wanted? (ex. '51' means at >=51% is language): ");
+            Integer percentLanguage = 0;
+            success = false;
+            while (!success) {
+                try {
+                    percentLanguage = Integer.valueOf(scn.next());
+                    success = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid, please enter a number (ex: '51'): ");
+                }
+            }
+            System.out.println("What is minimum amount of commits wanted? (ex. '300'): ");
+            Integer totalCommit = 0;
+            success = false;
+            while (!success) {
+                try {
+                    totalCommit = Integer.valueOf(scn.next());
+                    success = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid, please enter a number (ex: '300'): ");
+                }
+            }
+            System.out.println("What is minimum size in bytes of repo wanted? (ex. '5000'): ");
+            Integer totalSize = 0;
+            success = false;
+            while (!success) {
+                try {
+                    totalSize = Integer.valueOf(scn.next());
+                    success = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid, please enter a number (ex: '5000'): ");
+                }
+            }
+            System.out.println("What is start date of repos wanted? (ex. '2010-01-01'): ");
+            String sDate = "";
+            success = false;
+            while (!success) {
+                try {
+                    sDate = scn.next();
+                    success = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid, please enter a string (ex: '2010-01-01'): ");
+                }
+            }
 
-        while(menuChoice!=4){
-            System.out.println("** Menu: **" +
-                    "\n 1. Print all repos" +
-                    "\n 2. Clone all repos"+
-                    "\n 3. Export to CSV" +
-                    "\n 4. Exit\n");
-            System.out.println("Choice: ");
-            menuChoice = Integer.valueOf(scn.next());
-            switch(menuChoice){
-                case 1:
-                    System.out.println(rg.getRepos());
-                    break;
-                case 2:
-                    Clone.cloneRepos(rg.getRepos());
-                    break;
-                case 3:
-                    CSVManip.createCSV(rg.getRepos());
+            System.out.println("\n** Grabbing repos!");
+            RepoGrab rg = new RepoGrab(followers, languages, users, percentLanguage, totalCommit, totalSize, sDate);
+            System.out.println("\n** Grabbed repos!");
 
-                    // Uses given file name to create a txt file for query metadata
-                    try{
-                        FileWriter paramWrite = new FileWriter(CSVManip.fileName+ "_metadata.txt");
-                        paramWrite.write("+Min Followers: " + followers + "\n+Language: " + languages +
-                                "\n+Min Mentionable Users: " + users + " \n+Percent of Language: " + percentLanguage +
-                                "\n+Min Commits: " + totalCommit + "\n+Min Size in Bytes: " + totalSize + "\n+Start Date: " + sDate +
-                                "\n\n+Added Repos: " + rg.getAddedRepos() + "\n+Ignored Repos: " + rg.getIgnoredRepos() +
-                                "\n\n" + java.time.LocalDate.now() + " " + java.time.LocalTime.now());
-                        paramWrite.close();
-                    } catch(IOException e) {
-                        System.out.println("Parameter error");
-                    }
-                    break;
-                case 4:
-                    break;
+            while (menuChoice != 4 && menuChoice != 5) {
+                System.out.println("** Menu: **" +
+                        "\n 1. Print all repos" +
+                        "\n 2. Clone all repos" +
+                        "\n 3. Export to CSV" +
+                        "\n 4. New Query" +
+                        "\n 5. Exit \n");
+                System.out.println("Choice: ");
+                menuChoice = Integer.valueOf(scn.next());
+                switch (menuChoice) {
+                    case 1:
+                        System.out.println(rg.getRepos());
+                        break;
+                    case 2:
+                        Clone.cloneRepos(rg.getRepos());
+                        break;
+                    case 3:
+                        CSVManip.createCSV(rg.getRepos());
+
+                        // Uses given file name to create a txt file for query metadata
+                        try {
+                            FileWriter paramWrite = new FileWriter(CSVManip.fileName + "_metadata.txt");
+                            paramWrite.write("+Min Followers: " + followers + "\n+Language: " + languages +
+                                    "\n+Min Mentionable Users: " + users + " \n+Percent of Language: " + percentLanguage +
+                                    "\n+Min Commits: " + totalCommit + "\n+Min Size in Bytes: " + totalSize + "\n+Start Date: " + sDate +
+                                    "\n\n+Added Repos: " + rg.getAddedRepos() + "\n+Ignored Repos: " + rg.getIgnoredRepos() +
+                                    "\n\n" + java.time.LocalDate.now() + " " + java.time.LocalTime.now());
+                            paramWrite.close();
+                        } catch (IOException e) {
+                            System.out.println("Metadata output error");
+                        }
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        newQuery = false;
+                        break;
+                }
             }
         }
-
     }
 }
