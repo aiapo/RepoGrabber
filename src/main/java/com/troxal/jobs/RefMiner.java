b18e641b1a7ca5de6ba6f26b805f5555c807a032
aiapo <agitschl@umflint.edu>
2023-09-10 23:32:44 -0400

    All subtasks use WorkStealing
    So that way tasks get DONE

--- src/main/java/com/troxal/jobs/RefMiner.java
+++ src/main/java/com/troxal/jobs/RefMiner.java
@@ -1,7 +1,10 @@
 package com.troxal.jobs;
 
 import com.troxal.RepoGrab;
+import com.troxal.database.Database;
+import com.troxal.database.Manager;
 import com.troxal.manipulation.RefMine;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -9,9 +12,13 @@ public class RefMiner {
     public static void runJobs(RepoGrab repos){
         final Integer maxProcessors = Runtime.getRuntime().availableProcessors();
         final Integer totalThreadPool = maxProcessors-1;
+
         ExecutorService executor = Executors.newFixedThreadPool(totalThreadPool);
+        ExecutorService service = Executors.newWorkStealingPool();
+        Database db = new Manager().access();
+
         for(int j=0;j<repos.getRepos().size();j++){
-            executor.execute(new RefMine(repos.getRepo(j),false));
+            executor.execute(new RefMine(repos.getRepo(j),false,db,service));
         }
 
         // Shut down threads
@@ -20,6 +27,6 @@ public class RefMiner {
         while (!executor.isTerminated()) {
         }
 
-        System.out.println("\nFinished processing all repos!");
+        System.out.println("[INFO] Finished processing all repos!");
     }
 }
